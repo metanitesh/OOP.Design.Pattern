22de3126049ae62bf6337c7ed2dc15e720b2b732
Nitesh Sharma <nitesh.grep@gmail.com>
2021-09-27 11:27:24 +0530

    SRP solution

--- 01.principles/02.singleResponsibilityPrinciple.ts
+++ 01.principles/02.singleResponsibilityPrinciple.ts
@@ -5,19 +5,29 @@
 //  Created by Christopher Arriola on 1/22/18.
 //  Copyright © 2018 Udacity. All rights reserved.
 //
-
-
-class Game {
-  constructor(private score: number, private difficulty: number, private level: number) {
+class ScoreManager {
+  constructor(private score: number = 0) {
 
   }
 
-  backgroundColor() {
-    return this.level > 10 ? 'red' : 'green'
+  incrementScore(difficulty: number) {
+    this.score += 10 * difficulty
   }
 
-  incrementScore() {
-    this.score += 10 * this.difficulty
+  getScore() {
+    return this.score
+  }
+}
+
+class BackgroundManager {
+  backgroundColor(level: number) {
+    return level > 10 ? 'red' : 'green'
+  }
+}
+
+class LevelManager {
+  constructor(private level: number = 0, private difficulty: number = 1) {
+
   }
 
   updateLevel(level: number) {
@@ -28,9 +38,46 @@ class Game {
     this.difficulty = difficulty
   }
 
+  getLevel() {
+    return this.level
+  }
+
+  getDifficulty() {
+    return this.difficulty
+  }
+}
+
+class Game {
+  private scoreManager: ScoreManager
+  private backgroundManager: BackgroundManager
+  private levelManager: LevelManager
+  constructor(score: number, private difficulty: number, private level: number) {
+    this.scoreManager = new ScoreManager(score)
+    this.backgroundManager = new BackgroundManager()
+    this.levelManager = new LevelManager(level, difficulty)
+  }
+
+  backgroundColor() {
+    return this.backgroundManager.backgroundColor(this.levelManager.getLevel())
+  }
+
+  incrementScore() {
+    this.scoreManager.incrementScore(this.level)
+  }
+
+  updateLevel(level: number) {
+    this.levelManager.updateLevel(level)
+  }
+
+  updateDifficulty(difficulty: number) {
+    this.levelManager.changeDifficulty(difficulty)
+  }
+
+
 }
 
 const game = new Game(0, 2, 3)
 game.incrementScore()
+
 game.updateLevel(22)
 console.log(game.backgroundColor())
